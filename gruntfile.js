91f075358482c9cb64a3c5bed12a7a5147cc2525
João Victor Alves Teixeira <alvesteixeirajvat@gmail.com>
2024-09-22 14:11:05 -0300

    commit inicial

--- gruntfile.js
+++ gruntfile.js
@@ -36,6 +36,10 @@ module.exports = function(grunt){
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
                         }
                     ]
                 },
@@ -48,6 +52,28 @@ module.exports = function(grunt){
                     }
                 ]
             },
+            dev:{
+                options:{
+                    patterns:[
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand:true,
+                        flatten:true,
+                        src:['src/index.html'],
+                        dest:'dev/'
+                    }
+                ]
+            }
         },
         htmlmin:{
             dist:{
@@ -60,7 +86,14 @@ module.exports = function(grunt){
                 }
             }
         },
-        clean: ['prebuild']
+        clean: ['prebuild'],
+        uglify:{
+            target:{
+                files:{
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
@@ -68,7 +101,8 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
